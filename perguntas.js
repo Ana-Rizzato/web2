9cc9d09788cec17947979b28c3a60c5ca7189a37
Ana-Rizzato <anarizzato@prof.educacao.sp.gov.br>
2024-11-22 20:59:22 -0300

    Add files via upload
    aula da escola, projeto final do quarto bimestre

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'Quais são as partículas principais no estudo sobre radioatividade?',
+    'Partículas Alfa, Beta, Gama, Protons e Neutron'
+)
+
+criaCartao(
+    'Física',
+    'Qual átomo da tabela periódica representa uma partícula Alfa?',
+    'O átomo de Hélio que possui o número atômico igual a 2 e massa atómica igual a 4'
+)
+
+criaCartao(
+    'Física',
+    'O que é a fusão nuclear?',
+    'Fusão Nuclear é a união de pequenos núcleos atômicos, que formarão um núcleo maior e mais estável'
+)
+
+criaCartao(
+    'Física',
+    'O que é Fissão nuclear?',
+    'Ocorre quando um elemento pesado divide-se em outros dois elementos mais leves'
+)
